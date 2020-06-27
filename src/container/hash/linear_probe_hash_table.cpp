f9f577b35bbd7e0f479784d699dad4c1e9abee57
CtfChan <christophertzechan@gmail.com>
2020-06-27 08:55:49 +0800

    adding code for Insert

--- src/container/hash/linear_probe_hash_table.cpp
+++ src/container/hash/linear_probe_hash_table.cpp
@@ -89,37 +89,89 @@ bool HASH_TABLE_TYPE::GetValue(Transaction *transaction, const KeyType &key, std
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_TYPE::Insert(Transaction *transaction, const KeyType &key, const ValueType &value) {
-  // // Rlock table_latch
-  // table_latch_.RLock();
+  // Rlock table_latch
+  table_latch_.RLock();
 
-  // // Rlatch header page for this hash table
-  // Page* header_page = buffer_pool_manager_->FetchPage(header_page_id_);
-  // header_page->RLatch();
-  // HashTableHeaderPage* hash_header_page = 
-  //   reinterpret_cast<HashTableHeaderPage*>(header_page->GetData());
+  // Rlatch header page for this hash table
+  Page* header_page = buffer_pool_manager_->FetchPage(header_page_id_);
+  header_page->RLatch();
+  HashTableHeaderPage* hash_header_page = 
+    reinterpret_cast<HashTableHeaderPage*>(header_page->GetData());
 
-  // // compute hash idx, block idx and bucket idx 
-  // size_t hash_idx = hash_fn_.GetHash(key) % (BLOCK_ARRAY_SIZE * hash_header_page->NumBlocks());
-  // size_t block_idx = hash_idx / BLOCK_ARRAY_SIZE;
-  // size_t bucket_idx = hash_idx % BLOCK_ARRAY_SIZE;
+  // compute hash idx, block idx and bucket idx 
+  size_t hash_idx = hash_fn_.GetHash(key) % (BLOCK_ARRAY_SIZE * hash_header_page->NumBlocks());
+  size_t block_idx = hash_idx / BLOCK_ARRAY_SIZE;
+  size_t bucket_idx = hash_idx % BLOCK_ARRAY_SIZE;
 
-  // // get the block page, acquire write latch 
-  // Page* block_page = buffer_pool_manager_->FetchPage(hash_header_page->GetBlockPageId(block_idx));
-  // block_page->WLatch();
-  // auto hash_block_page = reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(block_page_all->GetData());
+  // get the block page, acquire write latch 
+  Page* block_page = buffer_pool_manager_->FetchPage(hash_header_page->GetBlockPageId(block_idx));
+  block_page->WLatch();
+  auto hash_block_page = reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(block_page->GetData());
   
-  // // keep trying to insert <key, val> into block
-  // while (hash_block_page->) {
+  // keep trying to insert <key, val> into block
+  while (hash_block_page->Insert(bucket_idx, key, value) == false) {
+    // duplicate values for same key are not allowed, insertion failed
+    if (comparator_(hash_block_page->KeyAt(bucket_idx), key) == 0 && hash_block_page->ValueAt(bucket_idx) == value) {
+      block_page->WUnlatch();
+      buffer_pool_manager_->UnpinPage(hash_header_page->GetBlockPageId(block_idx), true);
+      header_page->RUnlatch();
+      buffer_pool_manager_->UnpinPage(header_page_id_, false);
+      return false;
+    }
 
-  //   // duplicate values for same key are not allowed 
+    ++bucket_idx;
 
-  //   // resize table
+    // resize table when loop detected
+    if (block_idx * BLOCK_ARRAY_SIZE + bucket_idx == hash_idx) {
+      // release latches 
+      block_page->WUnlatch();
+      buffer_pool_manager_->UnpinPage(hash_header_page->GetBlockPageId(block_idx), true);
+      header_page->RUnlatch();
+      buffer_pool_manager_->UnpinPage(header_page_id_, false);
 
-  // }
+      // resize
+      Resize(hash_header_page->NumBlocks() * BLOCK_ARRAY_SIZE);
+
+      // reacquire latches
+      header_page = buffer_pool_manager_->FetchPage(header_page_id_);
+      header_page->RLatch();
+      hash_header_page = reinterpret_cast<HashTableHeaderPage*> (header_page->GetData());
+
+      // recompute idx
+      hash_idx = hash_fn_.GetHash(key) % (BLOCK_ARRAY_SIZE * hash_header_page->NumBlocks());
+      block_idx = hash_idx / BLOCK_ARRAY_SIZE;
+      bucket_idx = hash_idx % BLOCK_ARRAY_SIZE;
+
+      block_page = buffer_pool_manager_->FetchPage(hash_header_page->GetBlockPageId(block_idx));
+      block_page->WLatch();
+      hash_block_page = 
+        reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(block_page->GetData());
+      continue;
+    }
+
+    // done scanning current block, reset bucket_idx, block_idx and block_page
+    if (bucket_idx == BLOCK_ARRAY_SIZE) {
+      bucket_idx = 0;
+      
+      buffer_pool_manager_->UnpinPage(hash_header_page->GetBlockPageId(block_idx++), false);
+      if (block_idx == hash_header_page->NumBlocks()) 
+        block_idx = 0;
+
+      block_page->WUnlatch();
+      block_page = buffer_pool_manager_->FetchPage(hash_header_page->GetBlockPageId(block_idx));
+      hash_block_page = 
+        reinterpret_cast<HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(block_page->GetData());
+    }
+  }
 
   // release latches, locks and unpin 
+  block_page->WUnlatch();
+  buffer_pool_manager_->UnpinPage(hash_header_page->GetBlockPageId(block_idx), true);
+  header_page->RUnlatch();
+  buffer_pool_manager_->UnpinPage(header_page_id_, false);
+  table_latch_.RUnlock();
 
-  return false;
+  return true;
 }
 
 /*****************************************************************************
@@ -157,7 +209,71 @@ bool HASH_TABLE_TYPE::Remove(Transaction *transaction, const KeyType &key, const
  * RESIZE
  *****************************************************************************/
 template <typename KeyType, typename ValueType, typename KeyComparator>
-void HASH_TABLE_TYPE::Resize(size_t initial_size) {}
+void HASH_TABLE_TYPE::Resize(size_t initial_size) {
+  // write lock on table latch
+  table_latch_.WLock();
+
+  // allocate new header, acquire latch, update metadata
+  page_id_t old_header_page_id = header_page_id_;
+  auto new_page_header = buffer_pool_manager_->NewPage(&header_page_id_); 
+  new_page_header->WLatch();
+  auto new_hash_page_header = reinterpret_cast<HashTableHeaderPage*>(new_page_header->GetData());
+
+  size_t new_buckets = (2 * initial_size) / BLOCK_ARRAY_SIZE;
+  new_hash_page_header->SetSize(new_buckets);
+  new_hash_page_header->SetPageId(header_page_id_);
+
+  // allocate new blocks for new header
+  for (size_t bucket = 0; bucket < new_buckets; ++bucket) {   
+    page_id_t block_page_id = INVALID_PAGE_ID;
+    buffer_pool_manager_->NewPage(&block_page_id);
+    
+    // retry if allocation failed
+    if (block_page_id == INVALID_PAGE_ID) {
+      --bucket;
+      continue;
+    }
+
+    new_hash_page_header->AddBlockPageId(block_page_id);
+    buffer_pool_manager_->UnpinPage(block_page_id, false);
+  }
+
+  // insert <key, value> pairs into new blocks, delete olds ones
+  // get old header
+  Page* old_header_page = buffer_pool_manager_->FetchPage(old_header_page_id);
+  old_header_page->RLatch();
+  HashTableHeaderPage* old_hash_header_page = 
+    reinterpret_cast<HashTableHeaderPage*>(old_header_page->GetData());
+  
+  // iterate through each bucket of each block
+  for (size_t block = 0; block < old_hash_header_page->NumBlocks(); ++block) {
+    page_id_t block_page_id = old_hash_header_page->GetBlockPageId(block);
+    Page* page = buffer_pool_manager_->FetchPage(block_page_id);
+    page->RLatch();
+    auto page_block = reinterpret_cast
+      <HashTableBlockPage<KeyType, ValueType, KeyComparator> *>(page->GetData());
+    
+    for (size_t bucket_idx = 0; bucket_idx < BLOCK_ARRAY_SIZE; ++bucket_idx ) {
+      if (page_block->IsReadable(bucket_idx)) {
+        KeyType key = page_block->KeyAt(bucket_idx);
+        ValueType val = page_block->ValueAt(bucket_idx);
+        Insert(nullptr, key, val);
+      }
+    }
+
+    page->RUnlatch();
+    buffer_pool_manager_->UnpinPage(block_page_id, false); 
+    buffer_pool_manager_->DeletePage(block_page_id);
+  }
+
+
+  // let go of old header
+  old_header_page->RUnlatch();
+  buffer_pool_manager_->UnpinPage(old_header_page_id, false);
+  buffer_pool_manager_->DeletePage(old_header_page_id);
+  
+  table_latch_.WUnlock();
+}
 
 /*****************************************************************************
  * GETSIZE
