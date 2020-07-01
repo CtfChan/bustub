e62299cc23aa327daa53ebe8d14691c30e4bb611
CtfChan <christophertzechan@gmail.com>
2020-07-01 23:38:58 +0800

    seq scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -13,10 +13,23 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : 
+    AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() {}
+void SeqScanExecutor::Init() {
+  table_metadata_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+  table_iter_ = std::make_unique<TableIterator>(table_metadata_->table_->Begin(exec_ctx_->GetTransaction()));
+}
 
-bool SeqScanExecutor::Next(Tuple *tuple) { return false; }
+bool SeqScanExecutor::Next(Tuple *tuple) { 
+  auto end_iter = table_metadata_->table_->End();
+  while (*table_iter_ != end_iter) {
+    *tuple = **table_iter_;
+    ++(*table_iter_);
+    if (!plan_->GetPredicate() || plan_->GetPredicate()->Evaluate(tuple, &table_metadata_->schema_).GetAs<bool>()) 
+      return true;
+  }
+  return false;  
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -42,5 +42,8 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed. */
   const SeqScanPlanNode *plan_;
+
+  TableMetadata* table_metadata_;
+  std::unique_ptr<TableIterator> table_iter_; // TableIterator has no default constructor, so use ptr
 };
 }  // namespace bustub
